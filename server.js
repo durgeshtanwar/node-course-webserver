abaae31d53bcbaae1bc3865e484b304045457736
Tanwar Family <Tanwar Family>
2017-02-06 15:26:23 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use((req,res,next)=>{
 // app.use((req,res,next)=>{
 //     res.render('maintain.hbs');
 // })
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname +'/public'));
 app.get('/',(req,res)=>{
 res.render('home.hbs',{
     pageTitle: 'Home Page',
@@ -40,4 +41,6 @@ app.get('/bad',(req,res)=>{
     });
 })
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`server is up running on port no ${port}`);
+});
